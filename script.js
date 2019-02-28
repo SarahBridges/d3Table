0bb9ded5c7385ef4d09627819a7b8e4ec75e4073
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-02-28 14:55:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ build["Olin"] = {type: "math", floors: "three", year: "1986"};
 build["Young"] = {type: "science", floors: "four", year: "1970"};
 build["Crounse"] = {type: "history", floors: "four", year: "1967"};
 build["Grant"] = {type: "humanities", floors: "five", year: "1973"};
+
+var s = document.createElement("script");
+s.src="https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
+
+    var table = d3.select("body").append("table");
+    var rows = table.selectAll("tr").data(build).enter().append("tr");
+    rows.append("td").text(function(d){return d.type});
+    rows.append("td").text(function(d){return d.floors});
+    rows.append("td").text(function(d){return d.year});
+    </script>
