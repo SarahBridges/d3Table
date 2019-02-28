cd4ccbcc3e00b13ac4d5645c584b32ed845e1880
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-02-28 14:44:14 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var build = [];
+build["Olin"] = {type: "math", floors: "three", year: "1986"};
+build["Young"] = {type: "science", floors: "four", year: "1970"};
+build["Crounse"] = {type: "history", floors: "four", year: "1967"};
+build["Grant"] = {type: "humanities", floors: "five", year: "1973"};
