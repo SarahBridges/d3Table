bb94003721504e75c5f95e3d65f90c268eefa478
Newcomb <47224717+Newcomb@users.noreply.github.com>
2019-03-04 16:08:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var build = [];
-var Olin = {type: "math", floors: "three", year: "1986"};
-var Young = {type: "science", floors: "four", year: "1970"};
-var Crounse = {type: "history", floors: "four", year: "1967"};
-var Grant = {type: "humanities", floors: "five", year: "1973"};
-build.push(Olin,Young,Crounse,Grant)
+var Head = {name: "Name", type: "Curriculum", floors: "Floors", year: "Year Built"}
+var Olin = {name: "Olin" ,type: "math", floors: "three", year: "1986"};
+var Young = {name: "Young", type: "science", floors: "four", year: "1970"};
+var Crounse = {name: "Crounse", type: "history", floors: "four", year: "1967"};
+var Grant = {name: "Grant", type: "humanities", floors: "five", year: "1973"};
+build.push(Head,Olin,Young,Crounse,Grant)
 
 
     var table = d3.select("body").append("table");
@@ -11,6 +12,7 @@ build.push(Olin,Young,Crounse,Grant)
     .data(build)
     .enter()
     .append("tr");
+    rows.append("td").text(function(d){return d.name});
     rows.append("td").text(function(d){return d.type});
     rows.append("td").text(function(d){return d.floors});
     rows.append("td").text(function(d){return d.year});
