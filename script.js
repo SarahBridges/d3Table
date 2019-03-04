ede50b417f009cda2bd4e1f47522924da2671303
Newcomb Maddux <newcomb@Newcomb.local>
2019-03-04 16:50:51 -0500

    flooring

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var build = [];
 var Head = {name: "Name", type: "Curriculum", floors: "Floors", year: "Year Built"}
-var Olin = {name: "Olin" ,type: "Math", floors: "Three", year: "1986"};
-var Young = {name: "Young", type: "Science", floors: "Four", year: "1970"};
-var Crounse = {name: "Crounse", type: "History", floors: "Four", year: "1967"};
-var Grant = {name: "Grant", type: "Humanities", floors: "Five", year: "1973"};
+var Olin = {name: "Olin" ,type: "Math", floors: 3, year: 1986};
+var Young = {name: "Young", type: "Science", floors: 4, year: 1970};
+var Crounse = {name: "Crounse", type: "History", floors: 4, year: 1967};
+var Grant = {name: "Grant", type: "Humanities", floors: 5, year: 1973};
 build.push(Head,Olin,Young,Crounse,Grant)
 
 
@@ -12,7 +12,10 @@ build.push(Head,Olin,Young,Crounse,Grant)
     .data(build)
     .enter()
     .append("tr");
-    cols.append("td").text(function(d){return d.name}).style("border","solid 5px black");
+    cols.append("td").text(function(d){return d.name});
     cols.append("td").text(function(d){return d.type});
-    cols.append("td").text(function(d){return d.floors});
+    cols.append("td").text(function(d){return d.floors})
+    if (d.floors == 3){.style("background-color","blue")}
+    if (d.floors == 4){.style("background-color","green")}
+    if (d.floors == 5){.style("background-color","red")};
     cols.append("td").text(function(d){return d.year});
